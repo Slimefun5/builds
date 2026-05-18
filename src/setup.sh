34904f12d464cec7d8633590f0c5a8c6b7de769a
Intisy <finn@birich.de>
2026-05-18 15:53:12 +0200

    fix: clear checkout credential helper so ACCESS_TOKEN is used for push

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,5 @@
 git config user.name "TheBusyBot"
 git config user.email ${LOGIN_EMAIL}
 
-git remote set-url origin https://${ACCESS_TOKEN}@github.com/Slimefun5/builds.git
+git config --unset-all http.https://github.com/.extraheader || true
+git remote set-url origin https://x-access-token:${ACCESS_TOKEN}@github.com/Slimefun5/builds.git
